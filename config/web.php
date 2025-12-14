eeaffb80cdbcb404e1679567e13f5d63fc4bb70e
amirmahdishahbazi <amirmahdishahbazi1382@gmail.com>
2025-12-14 14:36:36 +0330

    fix sme configs

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'language' => 'fa-IR',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
--- models/User.php
+++ models/User.php
@@ -26,7 +26,7 @@ use yii\web\IdentityInterface;
 class User extends ActiveRecord implements IdentityInterface
 {
     const STATUS_DELETED = 0;
-    const STATUS_ACTIVE = 10;
+    const STATUS_ACTIVE = 1;
 
     // Define Role Constants
     const ROLE_ADMIN = 'admin';
@@ -37,7 +37,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return 'users';
+        return 'user';
     }
 
     public function behaviors()
